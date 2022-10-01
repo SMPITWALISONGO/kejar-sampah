fc2c8dfea34d9acfa2c666184cc64416d14831d1
SMPITWALISONGO <114791127+SMPITWALISONGO@users.noreply.github.com>
2022-10-01 10:34:15 +0700

    kejar sampah

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     let Hantu: Sprite = null
     info.changeScoreBy(1)
-    Hantu.setPosition(randint(0, 160), randint(0, 120))
+    Hantu.setPosition(randint(0, 150), randint(0, 120))
     info.startCountdown(10)
+})
+game.onUpdate(function () {
+	
 })
